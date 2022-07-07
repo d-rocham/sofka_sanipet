0f938ea7b45c07aa72e76837db3c85a47ff60277
d-rocham <d_rocham@outlook.com>
2022-07-07 16:06:30 -0500

    Added Doctor, Stylist classes

---
+++ src/person/Doctor.java
@@ -0,0 +1,7 @@
+package person;
+
+public class Doctor extends Employee{
+    public Doctor(String DNI, String name, String surname, String cellphone, int age) {
+        super(DNI, name, surname, cellphone, age);
+    }
+}
---
+++ src/person/Stylist.java
@@ -0,0 +1,7 @@
+package person;
+
+public class Stylist extends Employee {
+    public Stylist(String DNI, String name, String surname, String cellphone, int age) {
+        super(DNI, name, surname, cellphone, age);
+    }
+}
