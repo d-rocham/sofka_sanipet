231eaf3bcb7c4c37af576dd8a7d63988d81b4723
d-rocham <d_rocham@outlook.com>
2022-07-07 17:30:10 -0500

    Created SanipetClinic class w. main method

---
+++ src/sanipet/SanipetClinic.java
@@ -0,0 +1,9 @@
+package sanipet;
+
+public class SanipetClinic {
+    public static void main(String[] args) {
+        ClinicManager clinicSession = new ClinicManager();
+
+        // TODO: add CLI & other functionality.
+    }
+}
