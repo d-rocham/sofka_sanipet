9c9ac490ba1241799e89532d459ede5548008675
d-rocham <d_rocham@outlook.com>
2022-07-07 12:46:14 -0500

    created java project

---
+++ src/patient/Patient.java
@@ -0,0 +1,10 @@
+package patient;
+
+public abstract class Patient {
+    String type;
+    String historyNumber;
+    String name;
+    String breed;
+    String owner;
+    boolean vaccine_status;
+}
