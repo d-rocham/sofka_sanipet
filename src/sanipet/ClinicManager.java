9a2f4ae2fb63f1ad6195bd7c23c6543a9b5bdb8b
d-rocham <d_rocham@outlook.com>
2022-07-08 11:54:55 -0500

    Restored

--- src/sanipet/ClinicManager.java
+++ src/sanipet/ClinicManager.java
@@ -3,6 +3,7 @@ package sanipet;
 import medicine.medicine;
 import patient.Patient;
 import person.Employee;
+import person.Owner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,11 +13,14 @@ public class ClinicManager {
     List<medicine> medicines;
     List<Employee> personnel;
 
+    List<Owner> owners;
+
     // TODO: how to implement schedule?
 
     public ClinicManager() {
         this.patients = new ArrayList<>();
         this.medicines = new ArrayList<>();
         this.personnel = new ArrayList<>();
+        this.owners = new ArrayList<>();
     }
 }
--- src/sanipet/SanipetClinic.java
+++ src/sanipet/SanipetClinic.java
@@ -1,8 +1,15 @@
 package sanipet;
 
+import java.util.ArrayList;
+
 public class SanipetClinic {
     public static void main(String[] args) {
         ClinicManager clinicSession = new ClinicManager();
+
+
+
+        System.out.println("Sanipet - Veterinary care center");
+
 
         // TODO: add CLI & other functionality.
     }
