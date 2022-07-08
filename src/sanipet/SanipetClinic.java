985585bb64554c453a2c2ecba11b0ec7ca039263
d-rocham <d_rocham@outlook.com>
2022-07-08 12:18:19 -0500

    -

--- src/sanipet/SanipetClinic.java
+++ src/sanipet/SanipetClinic.java
@@ -6,7 +6,7 @@ public class SanipetClinic {
     public static void main(String[] args) {
         ClinicManager clinicSession = new ClinicManager();
 
-
+        // Menu
 
         System.out.println("Sanipet - Veterinary care center");
 
